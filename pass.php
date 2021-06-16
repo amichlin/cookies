3ab99ef1607e4e9ef16713f7eb3616ce5b5c386c
amichlin <amichlin@gmail.com>
2021-06-16 16:33:24 -0400

    Update pass.php

--- pass.php
+++ pass.php
@@ -3,21 +3,18 @@ $cookie_name = 'gandalf';
 $cookie_value = 'shallnotpass';
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
-if ( !IsSet($_COOKIE['gandalf']) )
+if ( !isset($_COOKIE['gandalf']) )
 {
-        echo 'Hmmmm... please reload your browser to continue<br />';
+	$_COOKIE['gandalf'] = 'shallnotpass';
 }
-else
-{
-        if ($_COOKIE['gandalf'] != 'shallnotpass')
-        {
-                echo 'pgCTF{goodenoughforme}';
-        }
-        else
-        {
-                echo 'You shall not pass!<br /><br/>';
-                echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/GxCplsdCwxY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-        }
-}
+	if ($_COOKIE['gandalf'] != 'shallnotpass')
+	{
+  		echo 'pgCTF{goodenoughforme}';
+	}
+	else
+	{
+  		echo 'You shall not pass!<br /><br/>';
+  		echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/GxCplsdCwxY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+	}
 
 ?>
